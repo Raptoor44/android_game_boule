2e62765fedf58300017e22d2c5b8662d143c1e05
Joachim Guitton (i201210) <i201210@iutlavalad.univ-lemans.fr>
2021-11-19 11:55:49 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android_game/MainActivity.java
@@ -0,0 +1,255 @@
+package com.example.android_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
+    private ImageView brique;
+    private RelativeLayout fenetrePrincipale;
+    private List<ImageView> list_brique;
+
+    private ImageView bande_noir;
+    private GestureDetectorCompat gestureDetector;
+
+    private ImageView boule;
+
+    private float xx;
+    private float yy;
+
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+
+    private boolean game = true;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //int variable :
+        list_brique = new ArrayList<ImageView>();
+
+
+        this.fenetrePrincipale = findViewById(R.id.fenetre_principale);
+
+        this.gestureDetector = new GestureDetectorCompat(this, this);
+        this.gestureDetector.setOnDoubleTapListener(this);
+
+        this.init_bandenoir();
+
+
+        //INIT THREAD
+        scheduler.scheduleAtFixedRate(runnable, 30, 30, TimeUnit.MILLISECONDS);
+
+        int x;
+        int y;
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                x = i * 90;
+                y = j * 90;
+
+                this.creer_brique(x, y);
+            }
+        }
+        this.init_boule();
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Insert custom code here
+
+            // Repeat every 2 seconds
+
+
+            boule.setY(boule.getY() + yy);
+            for (ImageView brique_for : list_brique) {
+                boolean hit_brique_boule;
+
+                hit_brique_boule = hitCheck(brique_for);
+
+                if (hit_brique_boule == true) {
+
+                    list_brique.remove(brique_for);
+                    fenetrePrincipale.removeView(brique_for);
+
+                    Log.e("co balles", "  " + boule.getX() + " + " + boule.getY());
+                    Log.e("co de la brique", "  " + brique_for.getX() + " + " + brique_for.getY());
+
+                }
+
+
+
+/*
+                for (int i = (int) brique_for.getX(); i <= (int) brique_for.getX() + 30; i++) {
+                    for (int j = (int) brique_for.getX(); j <= (int) brique_for.getX() + 30; j++) {
+                        if (i == boule.getX() && j == boule.getY() ){
+                            yy = 5;
+                            Log.e("PAS TOUCHE", "touche ALERTE");
+                        }else{
+                            yy = -5;
+                            Log.e("PAS TOUCHE", "");
+                        }
+                    }
+
+                }
+
+*/
+
+
+            }
+
+        }
+    };
+
+
+    private void init_boule() {
+        this.boule = new ImageView(this);
+        this.boule.setBackgroundResource(R.drawable.carre_bleu_ciel);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(50, 50);
+        this.boule.setLayoutParams(params);
+
+        this.boule.setX(500);
+        this.boule.setY(1300);
+
+        this.fenetrePrincipale.addView(boule);
+
+
+    }
+
+    private void init_bandenoir() {
+        this.bande_noir = new ImageView(this);
+        this.bande_noir.setBackgroundResource(R.drawable.bande_noir);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(100, 20);
+        this.bande_noir.setLayoutParams(params);
+
+        this.bande_noir.setX(500);
+        this.bande_noir.setY(1500);
+
+        this.fenetrePrincipale.addView(bande_noir);
+    }
+
+
+    private void creer_brique(int x, int y) {
+
+        brique = new ImageView(this);
+        brique.setBackgroundResource(R.drawable.brique);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(80, 80);
+        brique.setLayoutParams(params);
+
+        brique.setX(x);
+        brique.setY(y);
+
+        fenetrePrincipale.addView(brique);
+        list_brique.add(brique);
+    }
+
+    public boolean hitCheck(ImageView b) {
+
+
+        int x1 = (int) b.getX();
+        int width1 = b.getWidth();
+        int x2 = (int) boule.getX();
+        int width2 = boule.getWidth();
+        int y1 = (int) b.getY();
+        int height1 = b.getWidth();
+        int y2 = (int) boule.getY();
+        int height2 = boule.getHeight();
+
+
+        int right1 = x1 + width1;
+        int right2 = x2 + width2;
+        int bottom1 = y1 + height1;
+        int bottom2 = y2 + height2;
+
+        // Check if top-left point is in box
+        if (x2 >= x1 && x2 <= right1 && y2 >= y2 && y2 <= bottom1){
+            return true;
+        }else{
+            if (right2 >= x1 && right2 <= right1 && bottom2 >= y2 && bottom2 <= bottom1){
+                return true;
+            }else{
+                this.yy = -5;
+                return false;
+
+            }
+        }
+        // Check if bottom-right point is in box
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        int x = 0;
+        x = (int) e.getX();
+        this.bande_noir.setX(x);
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+}
